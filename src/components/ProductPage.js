0e3580fed26fecc0fb0a54b9af72770e12aa3db7
AndreyKh <andreykh69@gmail.com>
2022-11-24 09:53:47 +0200

    Remove dollar sign from product item page

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -13,7 +13,7 @@ function ProductItem() {
       <div className="itemInfo">
       <h4>{chosenProduct?.title}</h4>
         <h4>{chosenProduct?.brand}</h4>
-        <h4>$ {chosenProduct?.description}</h4>
+        <h4>{chosenProduct?.description}</h4>
       </div>
       <div className="itemImg">
         <img src={chosenProduct?.images?.[1]} alt={chosenProduct?.name} height="150" />
